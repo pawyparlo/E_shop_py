78543863fdf5b00208e4f5829266fb7eb449193f
pawelwyparlo <pawel.wyparlo@montrosesoftware.com>
2022-10-23 13:03:20 +0200

    Adding schemas for orders

--- e_shop/schema.py
+++ e_shop/schema.py
@@ -2,14 +2,15 @@ import graphene
 
 # import e_shop.shop.schema as shop
 from shop.schema import ProductsQuery
+from orders.schema import QueryOrder, CreateOrder
 
 
-class Query(ProductsQuery):
+class Query(ProductsQuery, QueryOrder):
     pass
 
 
-# class Mutation(shop.Mutation):
-#     pass
+class Mutation(CreateOrder):
+    pass
 
 
-schema = graphene.Schema(query=ProductsQuery)
+schema = graphene.Schema(query=Query, mutation=Mutation)
